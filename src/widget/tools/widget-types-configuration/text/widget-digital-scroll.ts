055c5c5045c24414a3c7d90cb399ebf6431e91f6
hejp <378540660@qq.com>
2022-08-16 15:56:39 +0800

    fix 修改滚动数字配置

--- src/widget/tools/widget-types-configuration/text/widget-digital-scroll.ts
+++ src/widget/tools/widget-types-configuration/text/widget-digital-scroll.ts
@@ -3,7 +3,7 @@
  * @Author: hejp
  * @Date: 2022-08-10 09:57:10
  * @Last Modified by: hejp
- * @Last Modified time: 2022-08-16 15:41:14
+ * @Last Modified time: 2022-08-16 15:55:54
  */
 import baseConfiguration from '../../base-configuration';
 const { font, data, box } = baseConfiguration;
@@ -13,32 +13,26 @@ console.log(box.configure);
 const widgetDigitalText = {
 	// 基础配置项
 	configure: [
-		[
-			{
-				name: '内部设置',
-				list: [
-					{
-						componentName: 'InputNumber',
-						label: '宽度',
-						name: 'styleWidth',
-						required: false,
-						placeholder: '请输入宽度',
-						min: 0,
-						max: 100
-					},
-					{
-						componentName: 'InputNumber',
-						label: '高度',
-						name: 'styleHeight',
-						required: false,
-						placeholder: '请输入高度',
-						min: 0,
-						max: 100
-					},
-					box.configure
-				]
-			}
-		]
+		...font.configure,
+		{
+			componentName: 'InputNumber',
+			label: '宽度',
+			name: 'styleWidth',
+			required: false,
+			placeholder: '请输入宽度',
+			min: 0,
+			max: 100
+		},
+		{
+			componentName: 'InputNumber',
+			label: '高度',
+			name: 'styleHeight',
+			required: false,
+			placeholder: '请输入高度',
+			min: 0,
+			max: 100
+		},
+		box.configure
 	],
 	data: data.configure
 };
