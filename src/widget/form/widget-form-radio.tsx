d672c6f9163c2ec94284a5dec4e673ad69577f10
hejp <378540660@qq.com>
2022-09-06 19:51:44 +0800

    fix 修改radio样式

--- src/widget/form/widget-form-radio.tsx
+++ src/widget/form/widget-form-radio.tsx
@@ -3,7 +3,7 @@
  * @Author: hejp 378540660@qq.com
  * @Date: 2022-08-28 14:00:20
  * @LastEditors: hejp 378540660@qq.com
- * @LastEditTime: 2022-09-06 17:31:00
+ * @LastEditTime: 2022-09-06 19:50:27
  * @FilePath: \bigscreen\src\widget\form\widget-form-radio.tsx
  * Copyright (c) 2022 by hejp email: 378540660@qq.com, All Rights Reserved.
  */
@@ -33,21 +33,26 @@ const WidgetFormRadio: FC<IWidgetFormRadioProps> = ({
       className='app-element app-element__radio animated'>
       <style
         dangerouslySetInnerHTML={{
-          __html: `.ant-radio-button-wrapper {
+          __html: `
+          .ant-radio-button-wrapper {
             background: ${options.radioBackgroundColor};
-          color:${options.radioColor};
-          border-color:${options.radioBorderColor};
-        }
-        .ant-radio-button-wrapper:hover{
-          color:${options.radioHighColor}!important;
-        }
-        .ant-radio-button-wrapper.ant-radio-button-wrapper-checked{
-          background: ${options.radioHighBackgroundColor}!important;
-          color:${options.radioHighColor}!important;
-          border-color:${options.radioHighBorderColor}!important;
-        }.ant-radio-button-wrapper:not(:first-child):before{
-          background:${options.radioBorderColor};
-        }`
+            color:${options.radioColor};
+            border-color:${options.radioBorderColor};
+          }
+          .ant-radio-button-wrapper:hover{
+            color:${options.radioHighColor}!important;
+          }
+          .ant-radio-button-wrapper.ant-radio-button-wrapper-checked{
+            background: ${options.radioHighBackgroundColor}!important;
+            color:${options.radioHighColor}!important;
+            border-color:${options.radioHighBorderColor}!important;
+          }
+          .ant-radio-button-wrapper:first-child{
+            border-left: 1px solid ${options.radioBorderColor};
+          }
+          .ant-radio-button-wrapper:not(:first-child):before{
+            background:${options.radioBorderColor};
+          }`
         }}></style>
       <Radio.Group
         buttonStyle='solid'
