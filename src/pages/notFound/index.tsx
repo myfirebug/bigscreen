2b7e9478105b08d180c5bc7ed9ce0924907a90a5
apple <ty-apple@mail.idcduty.com>
2023-08-31 16:21:04 +0800

    fix: 修改404页面

--- src/pages/notFound/index.tsx
+++ src/pages/notFound/index.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import "./index.scss";
+import { Button, Result } from "antd";
 
 function Home() {
-  return <div>notFound</div>;
+  return (
+    <Result
+      status="404"
+      title="404"
+      subTitle="您访问的页面走丢了~~~"
+      extra={<Button type="primary">返回上一页</Button>}
+    />
+  );
 }
 
 export default Home;
