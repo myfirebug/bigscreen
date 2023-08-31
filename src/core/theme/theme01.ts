ee8756427d414426b7cc8766073a23096021503a
apple <ty-apple@mail.idcduty.com>
2023-08-31 16:22:00 +0800

    fix: 修改主题

--- src/core/theme/theme01.ts
+++ src/core/theme/theme01.ts
@@ -8,15 +8,22 @@ export const theme01 = {
   /* ------------------切换白天夜间模式不变的变量end------------------ */
 
   /* ------------------切换白天夜间模式变化的变量start------------------ */
+
   // html背景颜色
   "--html-background-color": "#eee",
   // html字体颜色
   "--html-font-color": "#262626",
+  // 浅色字体颜色
+  "--light-font-color": "#999",
+  // 变色主色
+  "--change-primary-color": "#1677ff",
   // 浅色模块背景
   "--module-background-color": "#fff",
   // 深色模块背景
   "--module-dark-background-color": "#ccc",
   // 模块阴影
-  "--module-box-shadow-color": "rgba(0,0,0,.1)",
+  "--module-box-shadow-color": "rgba(0,0,0,.05)",
+  // 边框颜色
+  "--border-color": "#ddd",
   /* ------------------切换白天夜间模式变化的变量end------------------ */
 };
--- src/core/theme/theme01_dark.ts
+++ src/core/theme/theme01_dark.ts
@@ -12,11 +12,17 @@ export const theme01_dark = {
   "--html-background-color": "#222",
   // html字体颜色
   "--html-font-color": "#fff",
-  // 模块背景
-  "--module-background-color": "#000",
+  // 变色主色
+  "--change-primary-color": "#001529",
+  // 浅色字体颜色
+  "--light-font-color": "#999",
+  // 浅色模块背景
+  "--module-background-color": "#001529",
   // 深色模块背景
   "--module-dark-background-color": "#333",
   // 模块阴影
   "--module-box-shadow-color": "rgba(0,0,0,.1)",
+  // 边框颜色
+  "--border-color": "#333",
   /* ------------------切换白天夜间模式变化的变量end------------------ */
 };
